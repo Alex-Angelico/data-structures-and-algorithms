6379bafe3ad339dc9a6bbd633012afb7b1c5dfe7
Alex Angelico <alex.angelico@live.com>
2020-12-19 23:25:20 -0800

    challenge complete

---
+++ python/linked_list/README.md
@@ -0,0 +1,31 @@
+# README
+
+## Linked List Implementation
+
+### Author: Alex Angelico
+
+### Contributors: Anothy Beaver
+
+### Problem Domain
+
+Create a node calss  that has properties for the value stored in the node and a pointer to the next node.
+
+### Inputs and Expected Outputs
+
+Input | Expected Output
+----- | ---------------
+[3, 6, 10], 3 | '{10} -> {6} -> {3} -> NULL', True
+[None, 4], 99 | '{4} -> {None} -> NULL', False
+
+### Big O
+
+Time: O(1)  
+Space: O(N)
+
+### Whiteboard
+
+![Whiteboard](../assets/linked-list.jpg)
+
+### Change Log
+
+1.0: Completed Functionality - 19 Dec 2020
--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -1,12 +1,62 @@
+class Node:
+    """
+    Description
+    Creates and manipulates nodes
+
+    Methods
+    __init__(self, value): Creates node objects with data of value
+    getData(self): returns data
+    getNext(self): traverses to next node
+    """
+
+    def __init__(self, value=None):
+        self.data = value
+        self.next = None
+
+    def getData(self):
+        return self.data
+
+    def getNext(self):
+        return self.next
+
+
 class LinkedList:
     """
-    Put docstring here
+    Description
+    Creates and manipulates linked lists of nodes
+
+    Methods
+    __init__(self, value): Creates empty linked lists
+    insert(self, node): Adds a new node object to the start of a list
+    includes(self, value): Checks if at least one node in a linked list contains a specified value
+    __str__(self): Generates the linked values of a whole linked list object
     """
 
     def __init__(self):
-        # initialization here
-        pass
+        self.head = None
 
-    def some_method(self):
-        # method body here
-        pass
+    def insert(self, node):
+        node.next = self.head
+        self.head = node
+
+    def includes(self, value):
+        current = self.head
+        while current != None:
+            if current.getData() == value:
+                return True
+            else:
+                current = current.getNext()
+        return False
+
+    def __str__(self):
+        current = self.head
+        node_list = []
+        node_output = ''
+        while(current):
+            node_list.append(f"{ {current.data} } -> ")
+            current = current.next
+            if current == None:
+                node_list.append('NULL')
+        for node in node_list:
+            node_output += node
+        return node_output
