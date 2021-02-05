fdad59d31b6e52e42b4c7e1ad826881a88477067
Alex Angelico <alex.angelico@live.com>
2021-02-04 19:53:44 -0800

    challenge actually completed this time

--- python/code_challenges/tree/whiteboard-17.py
+++ python/code_challenges/tree/whiteboard-17.py
@@ -17,18 +17,28 @@ Space: O(1)
 
 # Algorithm
 1. Create a method within BinaryTree called breadthFirst() that takes no arguments.
-2. Us an appropriate configuration of loops and conditionals that cycles through each node in the tree breadth-first.
-3. Append node values to a list.
-4. Return the list.
+2. Create a node_list to hold node values.
+3. Create a node_queue to hold nodes.
+4. Make the roote node current.
+5. If current is empty, return 'Empty tree'.
+6. Enqueue current to node_queue.
+7. While there is a current node, dequeue the current node and append its value to node_list.
+8. Check for current.left and current.right nodes, and enqueue them in that order.
+9. If node_queue is empty, break the loop and return node_list.
+
 
 # Pseudocode
+
+
 # define breadthFirst method
 #   create a node_list variable that is an empty list
-#   append the root vaue of the tree to node_list
-#   define traverse helper function that accepts a root node
-#     if root.left and/or root.right exist, and add their values in order
-#     if root.left and/or root.right exist, traverse them in order
-#   traverse(self.root)
+#   creat a node_queue variable that is an empty queue
+#   current = self.root
+#   if current is none: return 'Empty tree'
+#   node_queue.enqueue(current)
+#   while there is a current node:
+#     if root.left and/or root.right exist, enqueue them in node_queue in that order
+#     if node_queue.is_empty(): break
 #   return node_list
 
 # Code
@@ -36,22 +46,21 @@ Space: O(1)
 
 def breadthFirst(self):
     node_list = []
+    node_queue = Queue()
+    current = self.root
+    if current == None:
+        return 'Empty tree'
 
-    node_list.append(self.root.value)
+    node_queue.enqueue(current)
 
-    def traverse(root):
+    while current:
+        current = node_queue.dequeue()
+        node_list.append(current.value)
+        if current.left:
+            node_queue.enqueue(current.left)
+        if current.right:
+            node_queue.enqueue(current.right)
+        if node_queue.is_empty():
+            break
 
-        if root.left:
-            node_list.append(root.left.value)
-
-        if root.right:
-            node_list.append(root.right.value)
-
-        if root.left:
-            traverse(root.left)
-
-        if root.right:
-            traverse(root.right)
-
-    traverse(self.root)
     return node_list
