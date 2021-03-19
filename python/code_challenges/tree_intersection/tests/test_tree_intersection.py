eef047d5fe6ce1a45b54434c190e28bcd5f69e73
Alex Angelico <alex.angelico@live.com>
2021-03-18 23:10:19 -0700

    additional test

--- python/code_challenges/tree_intersection/tests/test_tree_intersection.py
+++ python/code_challenges/tree_intersection/tests/test_tree_intersection.py
@@ -57,3 +57,32 @@ def test_both_trees_empty():
     actual = tree_intersection(tree1, tree2)
     expected = None
     assert actual == expected
+
+
+def test_huge_trees():
+    tree1 = BinarySearchTree()
+    tree2 = BinarySearchTree()
+
+    tree1.add(28)
+    tree1.add(17)
+    tree1.add(34)
+    tree1.add(4)
+    tree1.add(21)
+    tree1.add(33)
+    tree1.add(67)
+    tree1.add(155)
+    tree1.add(16)
+    tree1.add(2)
+
+    tree2.add(17)
+    tree2.add(16)
+    tree2.add(2)
+    tree2.add(67)
+    tree2.add(39)
+    tree2.add(8)
+    tree2.add(3)
+    tree2.add(94)
+
+    actual = tree_intersection(tree1, tree2)
+    expected = [2, 16, 17, 67]
+    assert actual == expected
